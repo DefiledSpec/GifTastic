c9dc804a147b06ddc73e7f0c9a58f7603c6c32b3
Jesse Harrah <defiledspec@gmail.com>
2018-06-14 15:45:12 -0500

    added giphy api

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,34 @@
-const APIKey = `p7EUf5FD5oJ7QMqy1k7MUXA6OYXpedP6`;
-let query = 'donald-trump'
-let queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&q=${query}&limit=10&offset=0&rating=G&lang=en`;
+// const APIKey = config.APIKey;
+// // let query = 'donald-trump'
 
-fetch(queryURL)
-    .then(res => res.json())
-    .then(data => {
-        let gifs = [...data.data];
-        gifs.forEach(gif => {
-            console.log(gif.images.original.url)
-            // console.log(gif.url)
-            $('.gifContainer')
-                .append($('<img>').attr('src', gif.images.original.url))
-        })
-    })
-    .catch(err => console.log(err))
+
+
+// function getGifs() {
+//     let query = $(this).attr('data-query') || 'cat';
+
+
+//     let queryURL = `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${APIKey}&limit=10&rating=pg`;
+//     //let queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&q=${query}&limit=10&offset=0&rating=G&lang=en`;
+//     fetch(queryURL)
+//         .then(res => res.json())
+//         .then(data => {
+//             let gifs = data.data;
+//             gifs.forEach(gif => {
+//                 console.log(gif.images.original.url)
+//                 console.log(gif.url)
+//                 switch(gif.rating){
+//                     case 'r':
+//                     case 'pg-13':
+//                         break;
+//                     default:
+//                         $('.gifContainer')
+//                             .append($('<p>').text(`Rating: ${gif.rating}`))
+//                             .append($('<img>').attr('src', gif.images.original.url))
+//                 }
+
+//             })
+//         })
+//         .catch(err => console.log(err))
+
+// }
+// $(document).on('click', '.gifBtn', getGifs);
