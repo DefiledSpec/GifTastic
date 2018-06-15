d93abdffef5fed84dad4c23601a86251a2942206
DefiledSpec <defiledspec@gmail.com>
2018-06-15 17:47:05 -0500

    added buttons, and functionallity to add new buttons, and search giphy with the content of the button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,46 +1,53 @@
-const APIKey = '&api_key=p7EUf5FD5oJ7QMqy1k7MUXA6OYXpedP6';
+const APIKey = 'p7EUf5FD5oJ7QMqy1k7MUXA6OYXpedP6';
 const host = 'https://api.giphy.com';
-const search = '/v1/gifs/search/q=';
+const search = '/v1/gifs/search?';
+const gifContainer = $('.gifContainer');
 
-let query = 'paul+rudd';
+function getGifs() {
+    let startTime = Date.now();
+    let query = $(this).attr('data-content').replace(' ', '_') || 'paul_rudd';
+    
+    let Q = query.replace('_', ' ');
+    console.log(`Fetching ${Q} from GIPHY`);
+    
+    let url = host + search;
+    url += $.param({
+        q: query,
+        limit: 10,
+        api_key: APIKey,
 
-let queryURL = host + search + query + APIKey; 
-
-$.ajax({url: queryURL, method: 'GET'})
-    .then(response => {
-        console.log(response)
     });
-// const APIKey = config.APIKey;
-// // let query = 'donald-trump'
+  
+    $.ajax({url: url, method: 'GET'})
+        .then(response => {
+            console.log(`Request ${Q} to GIPHY took ${((Date.now() - startTime)/1000).toFixed(2)} seconds to complete. Response:`, response.data)
+            displayGifs(response.data);
+        })
+        .catch(err => console.log(`Error: ${err}`));
+}
 
+function displayGifs(gifs) {
+    gifContainer.empty();
+    gifs.forEach(gif => {
 
-
-// function getGifs() {
-//     let query = $(this).attr('data-query') || 'cat';
-
-
-//     let queryURL = `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${APIKey}&limit=10&rating=pg`;
-//     //let queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&q=${query}&limit=10&offset=0&rating=G&lang=en`;
-//     fetch(queryURL)
-//         .then(res => res.json())
-//         .then(data => {
-//             let gifs = data.data;
-//             gifs.forEach(gif => {
-//                 console.log(gif.images.original.url)
-//                 console.log(gif.url)
-//                 switch(gif.rating){
-//                     case 'r':
-//                     case 'pg-13':
-//                         break;
-//                     default:
-//                         $('.gifContainer')
-//                             .append($('<p>').text(`Rating: ${gif.rating}`))
-//                             .append($('<img>').attr('src', gif.images.original.url))
-//                 }
-
-//             })
-//         })
-//         .catch(err => console.log(err))
-
-// }
-// $(document).on('click', '.gifBtn', getGifs);
+        switch(gif.rating){
+            case 'r':
+            case 'pg-13':
+            case 'nc-17':
+                // break
+            default:
+                gifContainer
+                    .append($('<p>').text(`Rating: ${gif.rating}`))
+                    .append($('<img>').attr('src', gif.images.downsized.url))
+        }
+    
+    })
+    
+}
+$('#addBtn').click(function(e) {
+    e.preventDefault();
+    let btnContent = $('#addBtnText').val();
+    if(!btnContent) { return console.log('Need Button Content')}
+    $('.btnGroup').append($('<button>').addClass('gifBtn').attr('data-content', btnContent).text(btnContent))
+})
+$(document).on('click', '.gifBtn', getGifs);
